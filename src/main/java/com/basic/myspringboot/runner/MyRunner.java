4d37db9f023fd7fc410a84b0075cc5238f90aba0
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-06 16:27:53 +0900

    MyRunner update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -5,6 +5,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
 @Component
 public class MyRunner implements ApplicationRunner {
     @Value("${myboot.name}")
@@ -17,8 +19,25 @@ public class MyRunner implements ApplicationRunner {
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
         System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
 
-        //Argument 목록 출력하기
+        /*
+            default void forEach(Consumer<? super T> action)
+            Consumer 인터페이스의 void accept(T t)
+         */
+        //Anonymous Inner Class
+        args.getOptionNames()
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
+
+        //Argument 목록 출력하기 (람다식)
         args.getOptionNames()//Set<String>
                 .forEach(name -> System.out.println(name));
+
+        //Methoer Reference (메서드 레퍼런스)
+        args.getOptionNames()//Set<String>
+                .forEach(System.out::println);
     }
 }
