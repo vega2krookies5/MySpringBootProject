5a824a0f6bd455714fa791f3f8b3fbfc1c216d5c
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-06 14:24:06 +0900

    Application update

--- src/main/java/com/basic/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootApplication.java
@@ -8,7 +8,11 @@ import org.springframework.context.annotation.Bean;
 public class MySpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootApplication.class, args);
+
+//        SpringApplication.run(MySpringBootApplication.class, args);
+        SpringApplication application = new SpringApplication(MySpringBootApplication.class);
+        application.run(args);
+
 	}
 
     @Bean
