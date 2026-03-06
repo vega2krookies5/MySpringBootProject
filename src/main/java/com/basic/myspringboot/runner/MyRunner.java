16eb76dd0de3048df573a2d8d2fb0f5eee747a8a
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-06 14:40:42 +0900

    MyRunner add

---
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -0,0 +1,13 @@
+package com.basic.myspringboot.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
