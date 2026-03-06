b2ccd10a896bc140eb79102151385920f22ccf15
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-06 15:39:36 +0900

    MyRunner update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,13 +1,18 @@
 package com.basic.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
         System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
 
