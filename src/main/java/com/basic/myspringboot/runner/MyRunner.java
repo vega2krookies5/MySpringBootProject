99fe638a3316a1651289aeafe798269d4cbe3337
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-06 16:39:32 +0900

    MyRunner update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,8 +1,10 @@
 package com.basic.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -15,10 +17,15 @@ public class MyRunner implements ApplicationRunner {
     @Value("${myboot.age}")
     private int age;
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("${myboot.name}  = " + name);
         System.out.println("${myboot.age}  = " + age);
+
+        System.out.println("${myboot.fullName}  = " + environment.getProperty("myboot.fullName"));
 
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
         System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
