687f2b4bb68738964b7f821be45eb07df78c3c1f
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-06 16:49:58 +0900

    dependency add

--- src/main/java/com/basic/myspringboot/property/MyBootProperties.java
+++ src/main/java/com/basic/myspringboot/property/MyBootProperties.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("myboot")
 public class MyBootProperties {
+    private String name;
+    private int age;
+    private String fullName;
 }
