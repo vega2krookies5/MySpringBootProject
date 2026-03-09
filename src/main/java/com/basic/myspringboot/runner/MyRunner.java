46d847ca8139fc059980a2ebd39d5b597692d6b9
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-09 13:19:22 +0900

    profile properties add

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -35,17 +35,18 @@ public class MyRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-         logger.info("현재 활성화된 CustomerVO = {}", customerVO);
-         logger.info("MyBootProperties getName() = {}", properties.getName());
-         logger.info("MyBootProperties getFullName() = {}", properties.getFullName());
+        System.out.println("Logger 구현체 클래스명 = " + logger.getClass().getName());
+        logger.info("현재 활성화된 CustomerVO = {}", customerVO);
+        logger.info("MyBootProperties getName() = {}", properties.getName());
+        logger.info("MyBootProperties getFullName() = {}", properties.getFullName());
 
-         logger.info("${myboot.name}  = {}", name);
-         logger.info("${myboot.age}  = {}", age);
+        logger.info("${myboot.name}  = {}", name);
+        logger.info("${myboot.age}  = {}", age);
 
-         logger.debug("DEBUG 레벨");
-         logger.debug(">>> ${myboot.fullName}  = {}", environment.getProperty("myboot.fullName"));
-         logger.debug("VM 아규먼트 foo : {}", args.containsOption("foo"));
-         logger.debug("Program 아규먼트 bar : {}", args.containsOption("bar"));
+        logger.debug("DEBUG 레벨");
+        logger.debug(">>> ${myboot.fullName}  = {}", environment.getProperty("myboot.fullName"));
+        logger.debug("VM 아규먼트 foo : {}", args.containsOption("foo"));
+        logger.debug("Program 아규먼트 bar : {}", args.containsOption("bar"));
 
         /*
             default void forEach(Consumer<? super T> action)
