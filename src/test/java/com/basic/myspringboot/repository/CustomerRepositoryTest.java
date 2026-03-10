bd2c1ca5e9a9422fc8ae2e5bf77d93ef7bfaf352
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-10 10:06:44 +0900

    Entity update

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.basic.myspringboot.repository;
 
+import com.basic.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +13,15 @@ class CustomerRepositoryTest {
     CustomerRepository customerRepository;
 
     //1. Customer 등록
-
+    @Test
+    void testCreate() {
+        //Given(준비단계)
+        Customer customer = new Customer();
+        customer.setCustomerId("A002");
+        customer.setCustomerName("스프링부트");
+        //When(실행단계)
+        Customer addCustomer = customerRepository.save(customer);
+        //Then(검증단계)
+    }
 
 }
