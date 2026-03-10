e4f3a9c0052e2ece6443b69c547851032e2b0455
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-10 09:52:37 +0900

    RepositoryTest add

---
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.basic.myspringboot.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+}
