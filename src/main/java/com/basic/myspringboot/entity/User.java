236cb0a1e85ed14bd586dd64e9bdb62ab9a829e1
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-10 15:48:06 +0900

    User entity add

---
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -0,0 +1,26 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
