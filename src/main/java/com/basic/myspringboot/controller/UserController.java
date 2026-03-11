d18ebf751864ab1b4c58bb8ee551578950cd4a0c
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-11 16:32:47 +0900

    leaf.html aa

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+    //static/index.html => UserController의 leaf() 호출 => templates/leaf.html
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name","스프링부트");
