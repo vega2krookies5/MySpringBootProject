4b91ca56cab52b418f0629927e9e459b05b945ee
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-11 16:27:29 +0900

    cotroller add

---
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.basic.myspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/thymeleaf")
+    public String leaf(Model model) {
+        model.addAttribute("name","스프링부트");
+        return "leaf";
+    }
+}
