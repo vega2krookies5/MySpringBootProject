1585fef5a880bdfccfde855bb54ae4061fa3c11e
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-11 11:22:17 +0900

    delete

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,12 +46,22 @@ public class UserRestController {
         return existUser;
     }
 
-    //Email 조회하고 수정하기
+    //Email 조회하고 User 수정하기
     @PatchMapping("/{email}/")
     public User updateUser(@PathVariable String email, @RequestBody User userDetail){
         User existUser = userRepository.findByEmail(email)
                 .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
         existUser.setName(userDetail.getName());
         return userRepository.save(existUser);
+    }
+
+    //User 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        userRepository.delete(user);
+        //return ResponseEntity.ok(user);
+        return ResponseEntity.ok().build();
     }
 }
