d3644354d766dd49f2076da5fb9fba7e1f02a4c3
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-11 14:36:33 +0900

    webconfig add

--- src/main/java/com/basic/myspringboot/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/basic/myspringboot/advice/DefaultExceptionAdvice.java
@@ -77,7 +77,7 @@ public class DefaultExceptionAdvice {
 
     // RuntimeException → Exception으로 변경
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorObject> handleException(Exception e) {
+    protected ResponseEntity<ErrorObject> handleException(RuntimeException e) {
         ErrorObject errorObject = new ErrorObject();
         // 예외 타입에 따라 적절한 status code 동적 결정
         HttpStatus status = resolveHttpStatus(e);
@@ -94,8 +94,8 @@ public class DefaultExceptionAdvice {
             return HttpStatus.UNSUPPORTED_MEDIA_TYPE; // 415
         } else if (e instanceof MissingServletRequestParameterException) {
             return HttpStatus.BAD_REQUEST; // 400
-        } else if (e instanceof NoResourceFoundException) {
-            return HttpStatus.NOT_FOUND; // 404
+//        } else if (e instanceof NoResourceFoundException) {
+//            return HttpStatus.NOT_FOUND; // 404
 //        } else if (e instanceof AccessDeniedException) {
 //            return HttpStatus.FORBIDDEN; // 403
         }
