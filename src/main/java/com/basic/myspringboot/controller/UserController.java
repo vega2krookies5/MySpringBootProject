2c6fa021a619bdf7224aba6ad111df663a8d1596
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-11 16:44:11 +0900

    usercontroller update

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
 
     //static/index.html => UserController의 leaf() 호출 => templates/leaf.html
     @GetMapping("/thymeleaf")
@@ -13,4 +17,6 @@ public class UserController {
         model.addAttribute("name","스프링부트");
         return "leaf";
     }
+
+    
 }
