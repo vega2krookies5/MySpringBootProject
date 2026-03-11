7e1a2afdc2590bd10c42e853147198fadf1f229e
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-11 16:51:15 +0900

    usercontroller update

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +22,21 @@ public class UserController {
         return "leaf";
     }
 
-    
+    /*
+    public ModelAndView(String viewName,
+                String modelName,
+                Object modelObject)
+     */
+    @GetMapping("/index")
+    public ModelAndView index() {
+        List<User> userList = userRepository.findAll();
+        return new ModelAndView("index","users",userList);
+    }
+
+//    @GetMapping("/index")
+//    public String index(Model model) {
+//        model.addAttribute("users", userRepository.findAll());
+//        return "index";
+//    }
+
 }
