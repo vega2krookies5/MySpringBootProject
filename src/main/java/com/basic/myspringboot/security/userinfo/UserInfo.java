042d09191139dddcf8eb8827f7a8d214b4df3cd3
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-12 16:54:58 +0900

    userinfo repo add

--- src/main/java/com/basic/myspringboot/security/userinfo/UserInfo.java
+++ src/main/java/com/basic/myspringboot/security/userinfo/UserInfo.java
@@ -1,18 +1,16 @@
 package com.basic.myspringboot.security.userinfo;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
@@ -20,7 +18,9 @@ public class UserInfo {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String roles;
 }
