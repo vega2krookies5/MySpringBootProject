9435a1ebabe9e16502439caaa812e023adae527f
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-12 11:09:37 +0900

    update-user.html

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -66,7 +66,7 @@ public class UserController {
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        model.addAttribute("user", user);
+        model.addAttribute("userForm", user);
         return "update-user";
     }
 
